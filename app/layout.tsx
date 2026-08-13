4d5073c74557218c5d140c4ac01c07798a557509
tommypagychoi <108319883+tommypagychoi@users.noreply.github.com>
2026-08-14 01:16:10 +0900

    Import StoryRoom generator global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./storyroom-builder.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://www.logoscloud.co.kr"),
